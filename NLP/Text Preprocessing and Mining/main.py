e137abf7af07fc7b06fcd1c7600a8a1a1a476bc0
Amit998 <damitlucky998@gmail.com>
2020-11-02 00:56:09 +0530

    data mining

--- NLP/Text Preprocessing and Mining/main.py
+++ NLP/Text Preprocessing and Mining/main.py
@@ -273,6 +273,270 @@ lst=[]
 
 # Count and Email Removal
 
-print(df[df['twitts'].str.contains('gmail.com')])
+# print(df[df['twitts'].str.contains('gmail.com')])
 
-print(df.iloc[2448]['twitts'])
+# print(df.iloc[3713]['twitts'])
+
+import re
+
+# x="@securerecs arghh Me Please  markbradbury_16@hotmail.com"
+
+# print(re.findall(r'([a-z0-9._+@]+[a-zA-Z]+\.[a-zA-Z]+\b)',x))
+
+# df['emails']=df['twitts'].apply(lambda x: re.findall(r'([a-z0-9._]+[a-zA-Z0-9]+\.[a-zA-Z0-9]+\b)',x))
+# df['twitts']=df['twitts'].apply(lambda x: re.sub(r'([a-z0-9._+]+[a-zA-Z0-9]+\.[a-zA-Z0-9]+\b)','',x))
+# 
+# df['email_count']=df['emails'].apply(lambda x: len(x))
+
+
+
+# print(df[df['email_count'] > 0].sample(10))
+#  print(df.sample(100))
+
+# Count URLs and Remove it
+
+
+# x='hello https://stackoverflow.com/questions/45913520/python-extract-urls-from-a-text-file-with-no-html-tags'
+# print(re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+','',x))
+
+# df['website_links']=df['twitts'].apply(lambda x: re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',x))
+
+# print(re.findall(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?',x))
+
+# df['website_count']=df['website_links'].apply(lambda x: len(x))
+
+# df['link_flags']=df['twitts'].apply(lambda x: re.findall(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?',x))
+
+
+# df['twitts']=df['twitts'].apply(lambda x: re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+','',x))
+
+
+
+# print(   df[df['website_count'] > 0 ].sample(10))
+
+
+
+
+# print(df['twitts'].str.contains('rt').sample(10))
+
+
+# x='rt @username: hello world'
+
+# print(re.sub(r'\brt\b','',x).strip())
+
+# df['twitts']=df['twitts'].apply(lambda x: re.sub(r'\brt\b','',x))
+
+# print(df.sample(10))
+
+
+
+
+##### Special Chars Removal or Punctuation removal
+
+# x='@JackAllTimeLow cant believe its endingg .. wh...  ...'
+# print(re.sub(r'[\w]+',"",x))
+# print(re.sub(r'[^\w]+'," ",x))
+
+# df['twitts']=df['twitts'].apply(lambda x: re.sub(r'[^\w]+'," ",x))
+
+
+## Remove Spaces
+
+x="Hi      Hello     how are    You"
+
+
+# print(x)
+# x=' '.join(x.split())
+# print(x)
+
+
+# df['twitts']=df['twitts'].apply(lambda x: ' '.join(x.split()))
+
+
+#### Remove Bewutifulsou4 
+
+x ='<html><h1>Thanks For Listing To Me</h1></html>'
+
+from bs4 import BeautifulSoup
+
+
+# x.replace('<html><h1>','')
+# print(x)
+
+# print(BeautifulSoup(x,'lxml').getText().strip())
+
+# print(x)
+
+# df['twitts']=df['twitts'].apply(lambda x: BeautifulSoup(x,'lxml').getText().strip())
+
+# print(df.sample(10))
+
+
+x= 'àèìòùÀÈÌÒÙ'
+
+import unicodedata
+
+
+def Remove_accented_Char(x):
+    x=unicodedata.normalize('NFKD',x).encode('ascii','ignore').decode('utf-8','ignore')
+    return x
+
+
+
+# df['twitts']=df['twitts'].apply(lambda x: Remove_accented_Char(x))
+
+
+### remove Stop Words
+
+
+# x ='this is stop words'
+
+# print(' '.join([t for t in x.split() if t not in stop_words]))
+
+
+# df['twitts_no_stop_word']=df['twitts'].apply(lambda x: ' '.join([t for t in x.split() if t not in stop_words]))
+
+# print(df.sample(10))
+
+# print(Remove_accented_Char(x))
+
+
+
+# Convert into base or root
+
+import spacy
+
+
+nlp=spacy.load('en_core_web_sm')
+
+x='chocolates balls times'
+
+
+def make_to_base(x):
+    x_list=[]
+    doc=nlp(x)
+    for token in doc:
+        lemma=str(token.lemma_)
+        if lemma == '-PRON-' or lemma == 'be':
+            x_list.append(lemma)
+    return ' '.join(x_list)
+
+# df['twitts']=df['twitts'].apply(lambda x: make_to_base(x))
+
+
+# print(df.sample(10))
+
+# print(make_to_base(x))
+
+
+## common word removal
+
+x="this is this okay bye"
+
+text=' '.join(df['twitts'])
+# print(len(text))
+
+text=text.split()
+
+# print(len(text))
+
+freq_comm=pd.Series(text).value_counts()
+
+# f20=freq_comm[0:20]
+# print(f20)
+
+
+# df['twitts']=df['twitts'].apply( lambda x:  ' '.join([ t for t in x if t not in f20 ])  )
+
+# text=' '.join(df['twitts'])
+# text=text.split()
+# freq_comm=pd.Series(text).value_counts()
+# f20=freq_comm[0:20]
+# print(f20)
+# print(freq_comm)
+
+
+# Rare Words
+
+
+print(freq_comm.tail())
+
+Rare20=freq_comm[-20:]
+# print(Rare20)
+
+
+# df['twitts']=df['twitts'].apply( lambda x:  ' '.join([ t for t in x if t not in Rare20 ])  )
+
+
+# print(freq_comm.tail())
+
+## Word Cloud Visualization
+
+
+# from wordcloud import WordCloud
+# import matplotlib.pyplot as plt
+# # %matplotlib inline
+# text=' '.join(df['twitts'])
+# print(len(text))
+
+# wc=WordCloud(width=800,height=400).generate(text)
+# plt.imshow(wc)
+# plt.axis('off')
+# plt.show()
+
+
+### Spelling Correction
+
+from textblob import TextBlob
+# x='Thanks for watchign it'
+
+# x=TextBlob(x).correct()
+# print(x)
+
+
+## Tokenization
+
+
+x="thanks#watching this. video"
+
+# print(TextBlob(x).words)
+
+# doc =nlp(x)
+
+# for token in doc:
+    # print(token)
+
+
+
+## Detecting Noun
+
+
+x="To type a lowercase character by using a key combination that includes the SHIFT key, hold down the CTRL+SHIFT+symbol keys simultaneously, and then release them before you type the letter. For example, to type a ô, hold down CTRL, SHIFT and ^, release and type o."
+
+
+# doc=nlp(x)
+
+
+# for noun in doc.noun_chunks:
+    # print(noun)
+
+
+
+# language Translation and Detaction
+
+
+tb=TextBlob(x)
+
+# print(tb.detect_language())
+
+# print(tb.translate(to='bn'))
+
+
+## Sentiment Classifire
+
+from textblob.sentiments import NaiveBayesAnalyzer
+
+z="we all stands together. we are gonna win this fight"
+
+demo=TextBlob(x,analyzer=NaiveBayesAnalyzer())
+print(demo.sentiment())
