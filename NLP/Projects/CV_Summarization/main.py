ff7f18a1f41a51038b19731a9d2ee674adb921d3
Amit998 <damitlucky998@gmail.com>
2020-12-09 19:45:05 +0530

    added CV Parsing

--- NLP/Projects/CV_Summarization/main.py
+++ NLP/Projects/CV_Summarization/main.py
@@ -2,14 +2,77 @@ import spacy
 import pickle
 import random
 
-from word2number import w2n
+
+train_data=pickle.load(open('Data/train_data.pkl','rb'))
 
 
-lst=['one','two','three']
+# print(train_data[0])
 
 
-for i in lst:
-    print(w2n.word_to_num(i))
+
+nlp=spacy.blank('en')
 
 
-df["columen_name"]=df["columen_name"].apply( lambda x: w2n.word_to_num(x) )
+def train_model(train_data):
+    if 'ner' not in nlp.pipe_names:
+        ner = nlp.create_pipe('ner')
+        nlp.add_pipe(ner, last = True)
+    
+    for _, annotation in train_data:
+        for ent in annotation['entities']:
+            ner.add_label(ent[2])
+    
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
+    with nlp.disable_pipes(*other_pipes):  # only train NER
+        optimizer = nlp.begin_training()
+        for itn in range(10):
+            print("Statring iteration " + str(itn))
+            random.shuffle(train_data)
+            losses = {}
+            index = 0
+            for text, annotations in train_data:
+                try:
+                    nlp.update(
+                        [text],  # batch of texts
+                        [annotations],  # batch of annotations
+                        drop=0.2,  # dropout - make it harder to memorise data
+                        sgd=optimizer,  # callable to update weights
+                        losses=losses)
+                except Exception as e:
+                    pass
+                
+            print(losses)
+
+
+
+# train_model(train_data)
+# nlp.to_disk('nlp_model')
+nlp_model=spacy.load('nlp_model')
+
+# print(train_data[0][0])
+
+# doc=nlp_model(train_data[0][0])
+# for ent in doc.ents:
+#     print((f'{ent.label_.upper():{30}} - {ent.text}'))
+
+import sys,fitz
+
+# fname='Data/Alice Clark CV.pdf'
+f2name="Data/AmitUpdated.pdf"
+
+doc=fitz.open(f2name)
+text=""
+
+for page in doc:
+    text=text+str(page.getText())
+
+
+tx=" ".join(text.split('\n'))
+# print(tx)
+
+
+doc=nlp_model(tx)
+for ent in doc.ents:
+    print((f'{ent.label_.upper():{30}} - {ent.text}'))
+
+
