101c171a7229a02e834949e4cfeda10fc2ebd751
Amit998 <damitlucky998@gmail.com>
2020-12-21 13:06:52 +0530

    updated gradient_descent

--- Machine Learning/Basic/gradient_descent.py
+++ Machine Learning/Basic/gradient_descent.py
@@ -1,20 +1,25 @@
 import numpy as np
 
 def gradient_descent(x,y):
-    m_curr=b_curr=0
-    iterations=10
-    learning_rate=0.001
+    coef_curr=intersect_curr=0
+    iterations=3000
+    learning_rate=0.01
 
     n=len(x)
     for i in range(iterations):
        
-        y_pred=m_curr * x + b_curr
-        md=-(2/n) * sum(x * (y-y_pred))
+        y_pred=coef_curr * x + intersect_curr
+        
+        coef_d=-(2/n) * sum(x * (y-y_pred))
+        intersect_d=-(2/n) * sum(y-y_pred)
+
+
         cost=(1/n) * sum([val**2 for val in (y-y_pred)])
-        bd=-(2/n) * sum(y-y_pred)
-        m_curr=m_curr - learning_rate * md
-        b_curr=b_curr-learning_rate * bd
-        print("M {}, B {},Cost {}, iteration {}".format(m_curr,b_curr,cost,i))
+        coef_curr=coef_curr - learning_rate * coef_d
+        intersect_curr=intersect_curr-learning_rate * intersect_d
+        # print("Coef {}, B {},Cost {}, iteration {}".format(coef_curr,intersect_curr,cost,i))
+    
+    return coef_curr,intersect_curr
 
 
 
@@ -22,4 +27,6 @@ def gradient_descent(x,y):
 x=np.array([1,2,3,5,6,7])
 y=np.array([6,7,8,9,10,11])
 
-gradient_descent(x,y)
+coef,intesect=gradient_descent(x,y)
+
+print(2 * coef+ intesect)
