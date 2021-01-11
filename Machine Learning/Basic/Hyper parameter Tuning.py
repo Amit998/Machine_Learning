c59e92e7a56c36c28ba0e80c81f422d23c0fdfd2
amit998 <damitlucky998@gmail.com>
2021-01-11 19:13:41 +0530

    added

--- Machine Learning/Basic/Hyper parameter Tuning.py
+++ Machine Learning/Basic/Hyper parameter Tuning.py
@@ -34,17 +34,73 @@ for kval in kernels:
 
 # print(avg_scores)
 
-from sklearn.model_selection import GridSearchCV
-clf = GridSearchCV(svm.SVC(gamma='auto'), {
-    'C': [1,10,20],
-    'kernel': ['rbf','linear']
-}, cv=5, return_train_score=False)
-clf.fit(iris.data, iris.target)
+# from sklearn.model_selection import GridSearchCV
+# clf = GridSearchCV(
+#     svm.SVC(gamma='auto'),
+#     {
+#     'C': [1,10,20],
+#     'kernel': ['rbf','linear']
+#     },
+#     cv=5, return_train_score=False
+# )
+# clf.fit(iris.data, iris.target)
 # print(clf.cv_results_)
 
 
-df = pd.DataFrame(clf.cv_results_)
-print(df)
-print(clf.best_params_)
-print(clf.best_score_)
+# df = pd.DataFrame(clf.cv_results_)
+# print(df[['param_C','param_kernel','mean_test_score']])
+# print(df.columns)
+# print(clf.best_params_)
+# print(clf.best_score_)
 
+
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+rc = RandomizedSearchCV(
+    svm.SVC(gamma='auto'),
+    {
+    'C': [1,10,20],
+    'kernel': ['rbf','linear']
+    },
+    cv=5,
+    return_train_score=False,
+    n_iter=2
+)
+rc.fit(iris.data, iris.target)
+
+
+df = pd.DataFrame(rc.cv_results_)
+# print(df[['param_C','param_kernel','mean_test_score']])
+# print(df.columns)
+# print(rc.best_params_)
+# print(rc.best_score_)
+
+
+model_params={
+    'svm':{
+        'model':svm.SVC(gamma='auto'),
+        'params':{
+            'C':[1,10,20],
+            'kernel':['rbf','linear']
+        }
+    },
+    'logistic_regression':{
+        'model':LogisticRegression(solver='liblinear',multi_class='auto'),
+        'params':{
+            'C':[1,5,10]
+        }
+    }
+}
+scores=[]
+
+for model_name,mp in model_params.items():
+    clf=GridSearchCV(mp['model'],mp['params'],cv=5,return_train_score=False)
+    clf.fit(iris.data,iris.target)
+    scores.append({
+        'model':model_name,
+        'best_score':clf.best_score_,
+        'best_params':clf.best_params_
+    })
+
+print(scores)
--- Machine Learning/Projects/Movie Recommendation Engine/main.py
+++ Machine Learning/Projects/Movie Recommendation Engine/main.py
@@ -38,23 +38,27 @@ cv=CountVectorizer()
 count_matrix=cv.fit_transform(df["combine_features"]).toarray()
 
 # print(count_matrix[0])
+
+# print(count_matrix[1])
 cosin_sim=cosine_similarity(count_matrix)
 
-# print(cosin_sim)
+# print(cosin_sim[1]) 
 movie='Maze Runner: The Scorch Trials'
 
 movie_index=get_index_from_title(movie)
-# print(movie_index)
+# print(cosin_sim[1])
 
 similer_movies=list(enumerate(cosin_sim[movie_index]))
+# print(similer_movies[movie_index])
 sorted_similer_movies=sorted(similer_movies,key=lambda x:x[1],reverse=True)
-# print(sorted_similer_movies)
+print(sorted_similer_movies[1][0])
+print(get_title_from_index(sorted_similer_movies[1][0]))
 
 
 # print(df["combine_features"].head())
-i=0
-for movie in sorted_similer_movies:
-    print(get_title_from_index(movie[0]))
-    i=i+1
-    if (i ==10):
-        break
+# i=0
+# for movie in sorted_similer_movies:
+#     print(get_title_from_index(movie[0]))
+#     i=i+1
+#     if (i ==10):
+#         break
