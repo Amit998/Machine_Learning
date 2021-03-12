ddf8404c9e7ef3546981a08277d2958715b93305
amit998 <damitlucky998@gmail.com>
2021-03-12 19:58:43 +0530

    added

--- Deep Learning/convolutional neural network(CNN)/projects/Real Time Face Mask Detection with Tensorflow and Python/main.py
+++ Deep Learning/convolutional neural network(CNN)/projects/Real Time Face Mask Detection with Tensorflow and Python/main.py
@@ -13,9 +13,11 @@ CONFIG_PATH=MODEL_PATH+'/my_ssd_mobnet/pipeline.config'
 
 CHECKPOINT_PATH=MODEL_PATH+'/my_ssd_mobnet/'
 
+CUSTOM_MODEL_NAME='my_ssd_mobnet'
+
 labels=[
-    {'name':'Mask','id':1},
-    {'name':'NoMask','id':2}
+    {'name':'mask','id':1},
+    {'name':'noMask','id':2}
 ]
 
 with open(ANNOTATION_PATH+'\label_map.pbtxt','w') as f:
@@ -27,4 +29,48 @@ with open(ANNOTATION_PATH+'\label_map.pbtxt','w') as f:
 
 
 # python Tensorflow/scripts/generate_tfrecord.py -x Tensorflow/workspace/images/train -l Tensorflow/workspace/annotations/label_map.pbtxt -o Tensorflow/workspace/annotations/train.record
+# python Tensorflow/scripts/generate_tfrecord.py -x Tensorflow/workspace/images/test -l Tensorflow/workspace/annotations/label_map.pbtxt -o Tensorflow/workspace/annotations/test.record
  
+
+import tensorflow as tf
+from object_detection.utils import config_util
+from object_detection.protos import pipeline_pb2
+from google.protobuf import text_format
+
+# from  object_detection.protos.DetectionModel import 
+
+# Tensorflow\workspace\models\pipeline.config
+
+CONFIG_PATH=MODEL_PATH+'/'+CUSTOM_MODEL_NAME+'/pipeline.config'
+config=config_util.get_configs_from_pipeline_file(CONFIG_PATH)
+ 
+
+
+pipeline_config=pipeline_pb2.TrainEvalPipelineConfig()
+with  tf.io.gfile.GFile(CONFIG_PATH,'r') as f:
+    proto_str=f.read()
+    text_format.Merge(proto_str,pipeline_config)
+
+
+
+pipeline_config.model.ssd.num_classes = 2
+pipeline_config.train_config.batch_size = 4
+pipeline_config.train_config.fine_tune_checkpoint = PRETRAINED_MODEL_PATH+'/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8/checkpoint/ckpt-0'
+pipeline_config.train_config.fine_tune_checkpoint_type = "detection"
+pipeline_config.train_input_reader.label_map_path= ANNOTATION_PATH + '/label_map.pbtxt'
+pipeline_config.train_input_reader.tf_record_input_reader.input_path[:] = [ANNOTATION_PATH + '/train.record']
+pipeline_config.eval_input_reader[0].label_map_path = ANNOTATION_PATH + '/label_map.pbtxt'
+pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[:] = [ANNOTATION_PATH + '/test.record']
+
+# print(config)
+
+config_text = text_format.MessageToString(pipeline_config)                                                                                                                                                                                                        
+with tf.io.gfile.GFile(CONFIG_PATH, "wb") as f:                                                                                                                                                                                                                     
+    f.write(config_text)
+
+print("""python {}/research/object_detection/model_main_tf2.py --model_dir={}/{} --pipeline_config_path={}/{}/pipeline.config --num_train_steps=5000""".format(APIMODEL_PATH, MODEL_PATH,CUSTOM_MODEL_NAME,MODEL_PATH,CUSTOM_MODEL_NAME))
+
+
+# python Tensorflow/models/research/object_detection/model_main_tf2.py --model_dir=Tensorflow/workspace/models/my_ssd_mobnet --pipeline_config_path=Tensorflow/workspace/models/my_ssd_mobnet/pipeline.config --num_train_steps=5000
+
+# from object_detection import mo
