0f14162f77313b02f4d420f98a338b4553b0483c
amit998 <damitlucky998@gmail.com>
2021-04-16 21:23:26 +0530

    added

--- Audio/Projects/EDA-Audio Classification Project Using Deep Learning/part2.py
+++ Audio/Projects/EDA-Audio Classification Project Using Deep Learning/part2.py
@@ -64,7 +64,7 @@ y=np.array(extracted_features_df['class'].tolist())
 
 
 
-y=pd.array(pd.get_dummies(y))
+y=np.array(pd.get_dummies(y))
 
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
@@ -100,3 +100,19 @@ model.add(Dense(num_labels))
 model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy',metrics=['accuracy'],optimizer='adam')
+
+from tensorflow.keras.callbacks import ModelCheckpoint
+from datetime import datetime
+
+num_epochs=100
+num_batch_size=32
+
+
+checkpointer=ModelCheckpoint(filepath='saved_model/audio_classification.hdf5',verbose=1,save_best_only=True)
+
+start=datetime.now()
+
+model.fit(x_train,y_train,batch_size=num_batch_size,epochs=num_epochs,validation_data=(x_test,y_test),callbacks=[checkpointer],verbose=1)
+duration=datetime.now-start
+
+print("Training completed in tie:",duration)
